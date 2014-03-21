4f7b9089a516f32cbbbc407484fefd0923bf237f
Juan Pablo Alperin <juan@alperin.ca>
2014-03-20 22:53:12 -0600

    working with MultiIndex pandas and hdf5 store

--- scielo.py
+++ scielo.py
@@ -7,15 +7,18 @@ from ratelimited import RateLimited
 
 from time import sleep, strftime, strptime
 import datetime
+import sys, os
 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-import cPickle
 
 import ConfigParser
 Config = ConfigParser.ConfigParser()
 Config.read('config.cnf')
+
+# use unbuffered stdout
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 ratelimited = RateLimited(10,1) # rate limit to 3 per second
 
@@ -92,14 +95,15 @@ def _make_date_from_ratchet_keys(y,m,d):
     to make a date object we need to strip the first char 
     turn into an int, and pass through date object
     '''
-    return datetime.date(int(y[1:]), int(m[1:]), int(d[1:]))
+    return datetime.datetime(int(y[1:]), int(m[1:]), int(d[1:]))
 
-def make_time_series(ratchet_response):
+def make_series(id, ratchet_response):
     ''' 
     Makes a Pandas Series relative to Jan 1, 2000 for easy comparisons
+    But keep the first date used and the ID
+    returns MultiIndex (days, id, start_date) Downloads
     '''
-    time0 = datetime.date(2000,1,1)
-    
+    print 'making series ...', 
     years = filter(lambda x: re.match('y\d\d\d\d', x), ratchet_response.keys())
     years.sort()
     
@@ -110,17 +114,32 @@ def make_time_series(ratchet_response):
     min_date = _make_date_from_ratchet_keys(min_year, min_month, min_day)
     
     days = []
-    accesses = []
+    accesses = {}
     for y in years:
         for m in filter(lambda x: re.match('m\d\d', x), ratchet_response[y].keys()):
             for d in filter(lambda x: re.match('d\d\d', x), ratchet_response[y][m].keys()):
                 current_date = _make_date_from_ratchet_keys(y, m, d)
                 day = (current_date-min_date).days  # number of days since first access
-                days.append(time0+datetime.timedelta(days=day)) # as day relative to time0
-                accesses[day] = ratchet_response[y][m][d]
+                # days.append(time0+datetime.timedelta(days=day)) # as day relative to time0
+                accesses[min_date+datetime.timedelta(days=day)] = ratchet_response[y][m][d]
 
-    return accesses
+    s = pd.Series(accesses.values(), index=accesses.keys())
+    s = s.sort_index()  # need to sort the series to avoid weirdness when working with time
+    start = s.idxmin()  # the first date for which there is data
+    df = pd.DataFrame(s)
+    df['id'] = str(id) # use str (not unicode) because hdf5 cannot handle unicode
+    df['start_date'] = start
+    df = df.reset_index()
+    df.columns = ['days', 'downloads', 'id', 'start_date']
+
+    # calculate the days since publication
+    df['days'] = df.days.map(lambda x:(x - start).days)    
     
+    df = df.set_index(['days', 'id', 'start_date'])
+    
+    print 'done'
+    return df.downloads
+
 def make_plot(accesses):
     colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
     fig, ax = plt.subplots()
@@ -135,26 +154,36 @@ def make_plot(accesses):
     ax.set_xlabel('Days since publication', fontsize=14)
     ax.set_ylabel('Number of downloads', fontsize=14)
     ax.set_title('Number of downloads per day since publication')
-    
-    
+        
     plt.show()
 
+series = []
 
-# content = fetch_scielo_identifiers()
-# content = fetch_scielo_article('S1679-39512005000300006')
+offset = 0;
+ids_fname = Config.get('files', 'idsfile')
+f = open(ids_fname, 'rb')
 
-# accesses = []
-# for id in [u'S0074-0276(00)09500214', u'S1516-3180(00)11800106', u'S0036-4665(00)04200103', u'S0036-4665(00)04200108', u'S0102-88392000000200008', u'S0074-0276(00)09500203', u'S0036-4665(00)04200110', u'S0100-72032006000800001', u'S1678-69712011000300006', u'S0074-0276(00)09500118']:
-#     downloads = fetch_downloads(id)
-#     if downloads:
-#         accesses.append(make_time_series(downloads))
-#     print id
-    
-# cPickle.dump(accesses, open('accesses.cPickle', 'wb'))
+fname = Config.get('files', 'h5file')
 
-# pre-loaded the ID's above
-# accesses = cPickle.load(open('accesses.cPickle'))
+os.remove(fname) # clear the file before starting (this will probably get me in trouble later)
+
+# create the store and append, using data_columns where I possibily
+# could aggregate
+with pd.get_store(fname) as store:
+    for l in f:
+        id = l.split(',')[0]
+        downloads = fetch_downloads(id)
+        if downloads:
+            store.append('s', make_series(id, downloads))
+        offset += 1
+        if (offset % 10) == 0:
+            print offset, id
+
+f.close()
+
+# This stuff might be useful for using the data        
+# s = pd.concat([data['S0036-4665(00)04200103'], data['S0036-4665(00)04200108']])
+# s.index = s.index.droplevel('start_date')
+# s.loc[s.index.get_level_values('days') < 30].unstack('id')        
+
 # make_plot(accesses)
-
-
-
