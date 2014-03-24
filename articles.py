a5a97a2cf9d92a9e85f1435ee1272802904387f8
Juan Pablo Alperin <juan@alperin.ca>
2014-03-24 13:45:39 -0600

    moved out article fetching code to new file

---
+++ articles.py
@@ -0,0 +1,82 @@
+import re, itertools, random
+import json
+import urllib, urllib2
+
+from xylose.scielodocument import Article
+from ratelimited import RateLimited
+
+from time import sleep, strftime, strptime
+import datetime
+import sys, os
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import cPickle
+
+import ConfigParser
+Config = ConfigParser.ConfigParser()
+Config.read('config.cnf')
+
+# use unbuffered stdout
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+
+ratelimited = RateLimited(10,1) # rate limit to 3 per second
+
+def fetch_scielo_identifiers():
+    base_url = Config.get('scielo_identifier', 'url').strip('/')
+    port = ':' + Config.get('scielo_identifier', 'port')
+    endpoint = Config.get('scielo_identifier', 'endpoint')
+    earliest = strptime(Config.get('ratchet', 'earliest_load'), '%Y-%m-%d')
+
+    offset = 0
+
+    data =  {'collection': 'scl'}
+    i = 0
+    all_ids = set()
+    while True:
+        data['offset'] = offset
+        url = base_url + port + endpoint + '?' + urllib.urlencode(data)
+        content = json.load(ratelimited.urlopen(url))
+
+        all_ids.update([obj['code'] for obj in content['objects']])
+        print "%s/%s - %.2f" % (len(all_ids), content['meta']['total'], (100 * len(all_ids) / float(content['meta']['total'])))
+        offset = content['meta']['offset'] + len(content['objects'])
+
+        # quit when we've reached the end
+        if offset > content['meta']['total']:
+            break
+
+    return all_ids
+
+def fetch_scielo_article(id):
+    base_url = Config.get('scielo_article', 'url').strip('/')
+    port = ':' + Config.get('scielo_article', 'port')
+    endpoint = Config.get('scielo_article', 'endpoint')
+    data = {'code': id}
+    url = base_url + port + endpoint + '?' + urllib.urlencode(data)
+    content = json.load(ratelimited.urlopen(url))
+
+    return Article(content)
+
+def loop_ids():
+    # grab each of the id's in the result
+    for object in content['objects']:
+        id = object['code']
+        article = fetch_scielo_article(id)
+        if not article:
+            continue
+        pub_date = article.publication_date
+        pub_date = pub_date + (10-len(pub_date))/3*'-01' # pad with -01 for complete publication date
+        pub_date = strptime(pub_date, '%Y-%m-%d')
+
+        if pub_date < earliest:
+            continue
+        i += 1
+        print i, strftime('%Y-%m-%d', pub_date)
+
+ids = fetch_scielo_identifiers()
+f = open(Config.get('files', 'datadir') + 'scielo_ids.cPickle')
+pickle.dump(ids, f)
+f.close()
--- scielo.py
+++ scielo.py
@@ -22,97 +22,48 @@ sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 
 ratelimited = RateLimited(10,1) # rate limit to 3 per second
 
-def fetch_scielo_identifiers():
-    base_url = Config.get('scielo_identifier', 'url').strip('/')
-    port = ':' + Config.get('scielo_identifier', 'port')
-    endpoint = Config.get('scielo_identifier', 'endpoint')
-    earliest = strptime(Config.get('ratchet', 'earliest_load'), '%Y-%m-%d')
-    
-    offset = 0
-    
-    data =  {'collection': 'scl'}
-    i = 0
-    while True:
-        data['offset'] = offset
-        url = base_url + port + endpoint + '?' + urllib.urlencode(data)
-        content = json.load(ratelimited.urlopen(url))
-    
-        offset = content['meta']['offset'] + len(content['objects'])
-  
-        # grab each of the id's in the result
-        for object in content['objects']:
-            id = object['code']
-            article = fetch_scielo_article(id)
-            if not article: 
-                continue
-            pub_date = article.publication_date
-            pub_date = pub_date + (10-len(pub_date))/3*'-01' # pad with -01 for complete publication date
-            pub_date = strptime(pub_date, '%Y-%m-%d')
-            
-            if pub_date < earliest:
-                continue
-            i += 1
-            print i, strftime('%Y-%m-%d', pub_date)
-            
-
-        # quit when we've reached the end
-        if offset > content['meta']['total']:
-            break
-            
-    return content
-
-def fetch_scielo_article(id):
-    base_url = Config.get('scielo_article', 'url').strip('/')
-    port = ':' + Config.get('scielo_article', 'port')
-    endpoint = Config.get('scielo_article', 'endpoint')
-    data = {'code': id}
-    url = base_url + port + endpoint + '?' + urllib.urlencode(data)
-    content = json.load(ratelimited.urlopen(url))
-
-    return Article(content)
-
 def fetch_downloads(id):
     base_url = Config.get('ratchet', 'url').strip('/')
     port = ':' + Config.get('ratchet', 'port')
     endpoint = Config.get('ratchet', 'endpoint')
-    
+
     data = {'code': id}
     url = base_url + port + endpoint + '?' + urllib.urlencode(data)
-    print 'fetching: ', id, '...', 
+    print 'fetching: ', id, '...',
     response = json.load(ratelimited.urlopen(url))
     print 'done'
     if not response:
         return None
-    
+
     # confirms these are all articles
     assert(response[0]['type'] == 'article')
-        
+
     return response[0]   #weirdly returns list instead of straight dict
-    
+
 def _make_date_from_ratchet_keys(y,m,d):
     '''
     Ratchet uses yXXXX, mXX, and dXX as strings
-    to make a date object we need to strip the first char 
+    to make a date object we need to strip the first char
     turn into an int, and pass through date object
     '''
     return datetime.datetime(int(y[1:]), int(m[1:]), int(d[1:]))
 
 def make_series(id, ratchet_response):
-    ''' 
+    '''
     Makes a Pandas Series relative to Jan 1, 2000 for easy comparisons
     But keep the first date used and the ID
     returns MultiIndex (days, id, start_date) Downloads
     '''
-    print 'making series ...', 
+    print 'making series ...',
     years = filter(lambda x: re.match('y\d\d\d\d', x), ratchet_response.keys())
     years.sort()
-    
+
     # We can do min because the 'total' key comes after m00 and d00 in the alphabet
     min_year = years[0]
     min_month = min(ratchet_response[min_year].keys())
     min_day = min(ratchet_response[min_year][min_month])
     min_date = _make_date_from_ratchet_keys(min_year, min_month, min_day)
-    
+
     days = []
     accesses = {}
     for y in years:
@@ -133,17 +84,17 @@ def make_series(id, ratchet_response):
     df.columns = ['days', 'downloads', 'id', 'start_date']
 
     # calculate the days since publication
-    df['days'] = df.days.map(lambda x:(x - start).days)    
-    
+    df['days'] = df.days.map(lambda x:(x - start).days)
+
     df = df.set_index(['days', 'id', 'start_date'])
-    
+
     print 'done'
     return df.downloads
 
 def make_plot(accesses):
     colors = itertools.cycle(['b', 'g', 'r', 'c', 'm', 'y', 'k'])
     fig, ax = plt.subplots()
-    for series in accesses: 
+    for series in accesses:
         print series
         ax.scatter(series.keys(), series.values(), c=next(colors))
 
@@ -154,7 +105,7 @@ def make_plot(accesses):
     ax.set_xlabel('Days since publication', fontsize=14)
     ax.set_ylabel('Number of downloads', fontsize=14)
     ax.set_title('Number of downloads per day since publication')
-        
+
     plt.show()
 
 series = []
@@ -176,21 +127,21 @@ with pd.get_store(fname) as store:
     for l in f:
         id = l.split(',')[0]
         downloads = fetch_downloads(id)
-        try: 
+        try:
             if downloads:
                 store.append('s', make_series(id, downloads), min_itemsize = 50)
-        except: 
+        except:
             print 'Problems with', id
-            
+
         offset += 1
         if (offset % 10) == 0:
             print offset, id
 
 f.close()
 
-# This stuff might be useful for using the data        
+# This stuff might be useful for using the data
 # s = pd.concat([data['S0036-4665(00)04200103'], data['S0036-4665(00)04200108']])
 # s.index = s.index.droplevel('start_date')
-# s.loc[s.index.get_level_values('days') < 30].unstack('id')        
+# s.loc[s.index.get_level_values('days') < 30].unstack('id')
 
 # make_plot(accesses)
